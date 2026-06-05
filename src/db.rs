6196eb4c14e83572691a4341e908572eedf3224a
PandaDEV <pandadev.net@proton.me>
2026-06-05 02:35:56 +0200

    fix(db): use AssertSqlSafe for dynamic query in sqlx 0.9

--- src/db.rs
+++ src/db.rs
@@ -20,7 +20,7 @@ pub async fn create_pool() -> Result<PgPool, sqlx::Error> {
 
     if !exists {
         let escaped = db_name.replace('"', "\"\"");
-        sqlx::query(&format!("CREATE DATABASE \"{escaped}\""))
+        sqlx::query(sqlx::AssertSqlSafe(format!("CREATE DATABASE \"{escaped}\"")))
             .execute(&admin)
             .await?;
     }
